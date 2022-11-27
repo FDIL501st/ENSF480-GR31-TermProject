aa72f26477c3ecff45688d854578128b6402049e
FDIL <fdil.fadilh1@gmail.com>
2022-11-26 18:20:47 -0700

    Update Movie.java
    Remove genre as an attribute as we currently are not using it.

--- Model/Movie.java
+++ Model/Movie.java
@@ -6,12 +6,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 public class Movie {
     private String movieName;
-    private String movieGenre;
     private Date releaseDate;
     private ArrayList<Date> availableTimes;
-    public Movie(String name,String genre,String releaseDate,ArrayList<String> times)  throws ParseException{
+    public Movie(String name,String releaseDate,ArrayList<String> times)  throws ParseException{
         movieName = name;
-        movieGenre = genre;
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy",Locale.ENGLISH);
         this.releaseDate =formatter.parse(releaseDate);
         formatter = new SimpleDateFormat("dd-MM-yyyy hh:mm",Locale.ENGLISH);
