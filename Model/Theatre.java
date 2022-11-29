4e4b8b80560077b56cee7f4c3bfcd5f6869701ef
Justin-kuhn <justin.kuhn2001@gmail.com>
2022-11-29 12:54:52 -0700

    Created Theatre.java

---
+++ Model/Theatre.java
@@ -0,0 +1,20 @@
+package Model;
+public class Theatre {
+    private String theatreName;
+    private String theatreLocation;
+    private int theatreID;
+	public static int theatreCount;
+    public Theatre(String name, String location){
+        this.theatreName = name;
+		this.theatreLocation = location;
+    }
+    public String getTheatreName(){
+        return this.theatreName;
+    }
+    public String getTheatreLocation(){
+        return this.theatreLocation;
+    }
+    public int getTheatreID() {
+        return this.theatreID;
+    }
+}
